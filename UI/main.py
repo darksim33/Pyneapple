f5c0bde0b8440b6597f3fc3f2b2fddacdfef3a09
darksim33 <57491748+darksim33@users.noreply.github.com>
2023-02-24 18:32:31 +0100

    Slice selection Update
    Added nslice Class to handle slice numbering;
    Added Callbacks for slider and spinbox;

--- UI/main.py
+++ UI/main.py
@@ -42,29 +42,55 @@ class nifti_img:
         return qpixmap
 
 
-def display_img(nii, axis):
-    slice = 10
+def display_img(nii, axis, slice):    
     axis.setPixmap(nii.nii2QPixmap(slice))
 
 
-class appData:
-    def __init__(self) -> None:
-        self.plot_boundries = np.array([0.0001, 0.2])
-        self.plot_slice = None
-        self.imgDyn = np.array()
-
+class appData():
+    def __init__(self):
+        self.plt_boundries = np.array([0.0001, 0.2])
+        self.plt_nslice = nslice()
+        self.imgDyn = None
+        
+class nslice():
+    def __init__(self):
+        self._value = None
+        self._number = None
+    @property
+    def number(self):
+        return self._number
+    @property
+    def value(self):
+        return self._value
+    @number.setter
+    def number(self, value):
+        self._number = value
+        self._value = value - 1
+    @value.setter
+    def value(self, value):
+        self._number = value + 1
+        self._value = value
 
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self) -> None:
         super(MainWindow, self).__init__()
-
-        self.appdata = appData
-
+        # Setup App
         self.setupUi(self)
         self.setWindowTitle("New NNLSDynApp")
+        self.AXImgDyn.setPixmap(QPixmap("ui\\resources\\image-not-available.jpg"))
+
+        # Prepare Classes
+        self.appdata = appData()
+        self.appdata.plt_nslice.number = self.spnBx_nSlice.value()
+        
+        # Setup Callbacks
         self.BttnImgDynLoad.clicked.connect(self.callback_BttnImgDynLoad)
-        self.AXImgDyn.setPixmap(QPixmap("ui\\resources\\test.jpg"))
-        # self.AXPltDyn.setPixmap(QPixmap("ui\\resources\\test.jpg"))
+        self.spnBx_nSlice.valueChanged.connect(self.callback_spnBX_nSlice_changed)
+        self.Sldr_nSlice.valueChanged.connect(self.callback_Sldr_nSlice_changed)
+
+        # Setup Defaults        
+        # self.txtEdt_BoundMin.setText(self.appdata.plt_boundries[0])
+        # self.txtEdt_BoundMax.setText(self.appdata.plt_boundries[1])
 
     def callback_BttnImgDynLoad(self):
         """fname = QtWidgets.QFileDialog.getOpenFileName(
@@ -72,7 +98,23 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         )"""
         fname = "pat01_img.nii"
         self.appdata.imgDyn = nifti_img(fname)
-        display_img(self.appdata.imgDyn, self.AXImgDyn)
+        self.appdata.plt_nslice.number = self.spnBx_nSlice.value()
+        display_img(self.appdata.imgDyn, self.AXImgDyn, self.appdata.plt_nslice.value)
+        self.spnBx_nSlice.setEnabled(True)
+        self.spnBx_nSlice.setMaximum(self.appdata.imgDyn.size[2])
+        self.Sldr_nSlice.setEnabled(True)
+        self.Sldr_nSlice.setMaximum(self.appdata.imgDyn.size[2])
+    
+    def callback_spnBX_nSlice_changed(self):
+        self.appdata.plt_nslice.number = self.spnBx_nSlice.value()
+        self.Sldr_nSlice.setValue(self.spnBx_nSlice.value())
+        display_img(self.appdata.imgDyn, self.AXImgDyn, self.appdata.plt_nslice.value)
+
+    def callback_Sldr_nSlice_changed(self):
+        self.appdata.plt_nslice.number = self.Sldr_nSlice.value()
+        self.spnBx_nSlice.setValue(self.Sldr_nSlice.value())
+        display_img(self.appdata.imgDyn, self.AXImgDyn, self.appdata.plt_nslice.value)
+
 
 
 app = QtWidgets.QApplication(sys.argv)
