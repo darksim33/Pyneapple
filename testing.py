db448ab68dbb69fa61fb1a4664827cf6c6f42a08
JJ <49498024+JoJas102@users.noreply.github.com>
2023-06-21 17:42:54 +0200

    Added testing file

---
+++ testing.py
@@ -0,0 +1,23 @@
+# from PineappleUI import startAppUI
+from pathlib import Path
+import utils as ut
+import fitting#, imantics
+# import matplotlib.pyplot as plt
+# import matplotlib.patches as patches  #
+# from PIL import Image
+
+# from plotting import Plot
+# import numpy as np
+from multiprocessing import freeze_support
+
+if __name__ == "__main__":
+    freeze_support()
+    img = ut.Nii(Path(r"data/01_img.nii"))
+    # img = ut.Nii(Path(r"data/pat16_img.nii.gz"))
+    # seg = ut.Nii_seg(Path(r"data/pat16_seg_test.nii.gz"))
+    seg = ut.Nii_seg(Path(r"data/01_mask_test.nii"))
+    # dyn = ut.Nii(Path(r"data/01_img_AmplDyn.nii"))
+
+    fit = fitting.FitData("NNLSreg", img, seg)
+    fit.fitting_pixelwise(debug=True)
+    print("Done")
