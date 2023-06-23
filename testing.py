df774072e9fd108f4914d3bc5d7324bbcac21fd2
JJ <49498024+JoJas102@users.noreply.github.com>
2023-06-23 08:59:30 +0200

    Formatting

--- testing.py
+++ testing.py
@@ -1,7 +1,7 @@
 # from PineappleUI import startAppUI
 from pathlib import Path
 import utils as ut
-from fit import *  # , imantics
+from fit import *
 
 # import matplotlib.pyplot as plt
 # import matplotlib.patches as patches  #
