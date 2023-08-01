b82465692c478b85b42247480813cbc0b73f60d5
JJ <49498024+JoJas102@users.noreply.github.com>
2023-08-01 08:48:20 +0200

    Wrote first approach of segmented fitting

--- fit.py
+++ fit.py
@@ -20,13 +20,24 @@ def fit_pixelwise(self, debug: bool = False):
     # TODO: add seg number utility
     pixel_args = self.fit_params.get_pixel_args(self.img, self.seg, debug)
     fit_function = self.fit_params.get_fit_function()
-    results_pixel = fit(fit_function, pixel_args, self.fit_params.n_pools, debug)
+    results = fit(fit_function, pixel_args, self.fit_params.n_pools, debug)
     self.fit_results = self.fit_params.eval_pixelwise_fitting_results(
-        results_pixel, self.seg
+        results, self.seg
     )
 
 
 # correct implementation of mean?
+# TODO: implement eval_segmented_fitting_results
+def fit_mean_seg(self):
+    pixel_args = self.fit_params.get_pixel_args(self.img, self.seg)
+    fit_function = self.fit_params.get_fit_function()
+    seg_args = np.mean(pixel_args, 3)
+    results = fit(fit_function, seg_args, self.fit_params.n_pools)
+    self.fit_results = self.fit_params.eval_segmented_fitting_results(
+        results, self.seg
+    )
+
+
 # def fit_seg_mean(signal: np.ndarray, fit_params: FitData.Parameters, seg_idx: int):
 #     if fit_params.model == FitModel.NNLS:
 #         basis = np.exp(
