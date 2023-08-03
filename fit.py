ecd53aacf75b6adeef9326d351f6f549115db329
JJ <49498024+JoJas102@users.noreply.github.com>
2023-08-03 08:39:26 +0200

    Implemented code for segmented fitting #5
    Check for correctness
    Testing needed
    
    Changed function order and slight improvements to code

--- fit.py
+++ fit.py
@@ -24,15 +24,25 @@ class FitData:
         else:
             print("Error: no valid Algorithm")
 
-    def fitting_pixelwise(self, debug: bool = False):
+    def fitting_pixelwise(self):
         # TODO: add seg number utility for UI purposes
         pixel_args = self.fit_params.get_pixel_args(self.img, self.seg)
         fit_function = self.fit_params.get_fit_function()
-        results_pixel = fit(fit_function, pixel_args, self.fit_params.n_pools, debug)
+        results = fit(fit_function, pixel_args, self.fit_params.n_pools)
         self.fit_results = self.fit_params.eval_pixelwise_fitting_results(
-            results_pixel, self.seg
+            results, self.seg
         )
 
+    ## NOTE: FOLLOWING CODE NOT IMPLEMENTED IN CODE STRUCTURE YET!
+    # TODO: correct implementation of mean? + testing
+    def fit_mean_seg(self):
+        pixel_args = self.fit_params.get_pixel_args(self.img, self.seg)
+        fit_function = self.fit_params.get_fit_function()
+        seg_args = np.mean(pixel_args, 3)
+        results = fit(fit_function, seg_args, self.fit_params.n_pools)
+        self.fit_results = self.fit_params.eval_segmented_fitting_results(
+            results, self.seg
+        )
 
 def fit(fitfunc, pixel_args, n_pools, debug: bool | None = False):
     # Run Fitting
@@ -49,43 +59,6 @@ def fit(fitfunc, pixel_args, n_pools, debug: bool | None = False):
     return results_pixel
 
 
-## NOTE: FOLLOWING CODE NOT IMPLEMENTED IN CODE STRUCTURE YET!
-def fit_pixelwise(fit_data, debug: bool = False):
-    # TODO: add seg number utility
-    pixel_args = fit_data.fit_params.get_pixel_args(fit_data.img, fit_data.seg, debug)
-    fit_function = fit_data.fit_params.get_fit_function()
-    results = fit(fit_function, pixel_args, fit_data.fit_params.n_pools, debug)
-    fit_data.fit_results = fit_data.fit_params.eval_pixelwise_fitting_results(
-        results, fit_data.seg
-    )
-
-
-# correct implementation of mean?
-# TODO: implement eval_segmented_fitting_results
-def fit_mean_seg(self):
-    pixel_args = self.fit_params.get_pixel_args(self.img, self.seg)
-    fit_function = self.fit_params.get_fit_function()
-    seg_args = np.mean(pixel_args, 3)
-    results = fit(fit_function, seg_args, self.fit_params.n_pools)
-    self.fit_results = self.fit_params.eval_segmented_fitting_results(
-        results, self.seg
-    )
-
-
-# def fit_segmentation_signal(signal: np.ndarray, fit_params: FitData.Parameters, seg_idx: int):
-#     if fit_params.model == FitModel.NNLS:
-#         basis = np.exp(
-#             -np.kron(
-#                 fit_params.b_values.T,
-#                 fit_params.get_bins(),
-#             )
-#         )
-#         fit_function = partial(fit_params.model, basis=basis)
-#     elif fit_params.model == FitModel.mono:
-#         print("test")
-#     return fit_function(seg_idx, signal)
-#
-#
 # def setup_signalbased_fitting(fit_data: FitData):
 #     img = fit_data.img
 #     seg = fit_data.seg
