7ae9252264e3a2437d53123ab74f999d4558ab63
JJ <49498024+JoJas102@users.noreply.github.com>
2023-08-21 14:46:11 +0200

    Corrected multi_exp_printer() and fixed multi_exp() fitting

--- src/fit/model.py
+++ src/fit/model.py
@@ -75,38 +75,38 @@ class Model(object):
     ):
         """Multi-exponential fitting model (for non-linear fitting methods and algorithms)"""
 
-        # TODO: working? testing needed
         def multi_exp_wrapper(n_components: int):
             def multi_exp_model(b_values, *args):
                 f = 0
-                for i in range(1, n_components):
+                for i in range(n_components - 1):
                     f += (
                         np.exp(-np.kron(b_values, abs(args[i])))
                         * args[n_components + i]
                     )
 
                 f += (
-                    np.exp(-np.kron(b_values, abs(args[n_components])))
-                    # Last n_components entries containing f, except for S0 as the last entry
-                    * (1 - (np.sum(args[-n_components:-1])))
+                    np.exp(-np.kron(b_values, abs(args[n_components - 1])))
+                    # Last entries containing f, except for S0 as the last entry
+                    * (1 - (np.sum(args[n_components:-1])))
                 )
 
                 if TM is not None and not 0:
-                    f *= np.exp(-args[2] / TM)
+                    # With second-last entry being T1 in cases of T1 fitting
+                    f *= np.exp(-args[-2] / TM)
 
                 return f * args[-1]  # Add S0 term for non-normalized signal
 
             return multi_exp_model
 
-        # def multi_exp_printer(n_components: int):
-        #     def multi_exp_model(b_values, x0):
-        #         f = f"0 + "
-        #         for i in range(n_components - 1):
-        #             f += f"np.exp(-np.kron(b_values, abs(x0[{i}]))) * x0[{n_components} + {i}] + "
-        #         f += f"np.exp(-np.kron(b_values, abs(x0[{n_components - 1}]))) * (100 - (np.sum(x0[n_components:])))"
-        #         return f"( " + f + f" ) * x0({n_components} + {i} + {1})"
-        #
-        #     return multi_exp_model
+        def multi_exp_printer(n_components: int):
+            def multi_exp_model(*args):
+                f = f""
+                for i in range(n_components - 1):
+                    f += f"exp(-kron(b_values, abs({args[i]}))) * {args[n_components + i]} + "
+                f += f"exp(-kron(b_values, abs({args[n_components-1]}))) * (1 - (sum({args[n_components:-1]})))"
+                return f"( " + f + f" ) * {args[-1]}"
+
+            return multi_exp_model
 
         fit = curve_fit(
             multi_exp_wrapper(n_components),
