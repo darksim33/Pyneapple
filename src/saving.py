1460c4e646fcf76e0d923dcf6ba8f95aae8c38fa
JJ <49498024+JoJas102@users.noreply.github.com>
2023-11-24 12:45:48 +0100

    Corrected results structure

--- src/saving.py
+++ src/saving.py
@@ -21,24 +21,24 @@ def set_up_results_struct(data: FitData):
     index = 0
     current_pixel = 0
 
-    for key, value in data.fit_results.d.items():
-        n_comps = len(value)
+    for key, d_values in data.fit_results.d.items():
+        n_comps = len(d_values)
         current_pixel += 1
 
-        for comp in range(0, n_comps):
+        for comp, d_comp in enumerate(d_values):
             # Implement saving of multiple methods into one file? May be more convenient,
             # but single files are clearer, don't need caching and no further coding needed
 
             index += 1
-            result_dict[key] = {
+            result_dict[key + (comp + 1,)] = {
                 # 'ROI': 0,
+                "index": index,
                 "pixel": current_pixel,
                 "pixel_position": key,
                 "method": data.model_name,
-                "compartment": comp + 1,
-                "D": value[comp],
+                "D": d_comp,
                 "f": data.fit_results.f[key][comp],
-                "foundCompartments": n_comps,
+                "found_compartments": n_comps,
             }
 
     return result_dict
