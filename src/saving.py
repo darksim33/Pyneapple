a729525b4fe2fba6ec405e97c1affa58a5c78a4e
JJ <49498024+JoJas102@users.noreply.github.com>
2023-11-28 07:47:11 +0100

    Optimised result structure
    Key index containing pixel position, slice number and compartment now implemented into df with own column each

--- src/saving.py
+++ src/saving.py
@@ -5,11 +5,12 @@ from src.utils import Nii
 
 
 def save_results(data: FitData):
-    result_dict = set_up_results_struct(data)
+    result_df = pd.DataFrame(set_up_results_struct(data)).T
 
-    # Save results for D and f to Excel sheet
-    result_df = pd.DataFrame(result_dict).T
-    test = result_df.reset_index()
+    # Restructure key index and save results
+    result_df.reset_index(
+        names=["pixel_x", "pixel_y", "slice", "compartment"], inplace=True
+    )
     result_df.to_excel(Path(f"data/results/PyNeapple_results_{data.model_name}.xlsx"))
 
     # Save spectrum as Nii
@@ -19,7 +20,6 @@ def save_results(data: FitData):
 
 def set_up_results_struct(data: FitData):
     result_dict = {}
-    index = 0
     current_pixel = 0
 
     for key, d_values in data.fit_results.d.items():
@@ -27,19 +27,12 @@ def set_up_results_struct(data: FitData):
         current_pixel += 1
 
         for comp, d_comp in enumerate(d_values):
-            # Implement saving of multiple methods into one file? May be more convenient,
-            # but single files are clearer, don't need caching and no further coding needed
-
-            index += 1
             result_dict[key + (comp + 1,)] = {
-                # "index": index,
                 "element": current_pixel,
-                "pixel_position": key,  # Only available for pixel-wise fitting?
-                "compartment": comp + 1,
                 "method": data.model_name,
                 "D": d_comp,
                 "f": data.fit_results.f[key][comp],
-                "found_compartments": n_comps,
+                "n_compartments": n_comps,
             }
 
     return result_dict
