9313676c888469b0b18dbb96fc503cc85e264ee4
JJ <49498024+JoJas102@users.noreply.github.com>
2023-12-12 08:03:13 +0100

    Finished script for evaluation of result data

--- data/testing/evaluate_results.R
+++ data/testing/evaluate_results.R
@@ -11,11 +11,11 @@ library(colorspace)
 # import and set up data
 results_df = read_excel(path="test_img_176_176_NNLS_results_bottom.xlsx")
 n_comps = max(results_df$n_compartments)
-eval_df = data.frame(compartment=c(1:n_comps))
+eval_df = data.frame(compartment=c(1:n_comps), D_mean=0, f=0)
 
 ## subset data frame to calculate mean per compartment and parameter
 for (comp in 1:n_comps) {
   results_comp = subset(results_df, compartment == comp, select = c("D", "f"))
-  eval_df[c("compartment", "D-mean", "f_mean")] = c(comp, mean(results_comp$D), mean(results_comp$f))
-  print(eval)
+  eval_df[comp,] = c(comp, mean(results_comp$D), mean(results_comp$f))
 }
+print(eval_df)
