4fa3065e9b7b22ecd4e383e46564092ee397c11f
JJ <49498024+JoJas102@users.noreply.github.com>
2023-12-12 12:16:14 +0100

    Finished script to evaluate multiple analysis/methods reults

--- data/testing/evaluate_results.R
+++ data/testing/evaluate_results.R
@@ -7,15 +7,32 @@ library(see)
 library(ggtext)
 library(colorspace)
 
-
-# import and set up data
-results_df = read_excel(path="test_img_176_176_NNLS_results_bottom.xlsx")
-n_comps = max(results_df$n_compartments)
-eval_df = data.frame(compartment=c(1:n_comps), D_mean=0, f=0)
-
-## subset data frame to calculate mean per compartment and parameter
-for (comp in 1:n_comps) {
-  results_comp = subset(results_df, compartment == comp, select = c("D", "f"))
-  eval_df[comp,] = c(comp, mean(results_comp$D), mean(results_comp$f))
+# Function to calculate mean d and f values for given result data frame
+evaluate_results = function(result_df){
+  ## Initialize data frame
+  n_comps = max(result_df$n_compartments)
+  eval_df = data.frame(compartment=c(1:n_comps), d_mean=0, f=0)
+  
+  ## Subset data frame to calculate mean per compartment and parameter
+  for (comp in 1:n_comps) {
+    results_comp = subset(result_df, compartment == comp, select = c("D", "f"))
+    eval_df[comp,] = c(comp, mean(results_comp$D), mean(results_comp$f))
+  }
+  return(eval_df)
 }
-print(eval_df)
+
+
+# Main script
+## Import Excel files and set up data
+file.list = list.files(path = ".", pattern = "*.xlsx", full.names = TRUE)
+results_df = lapply(file.list, read_excel)
+eval_results = list()
+
+## Loop over analysed methods in form of Excel sheets loaded
+for (method in seq_along(results_df)) {
+  eval_results[[method]] = evaluate_results(results_df[[method]])
+  
+  print(file.list[method])
+  print(eval_results[[method]])
+  
+}
