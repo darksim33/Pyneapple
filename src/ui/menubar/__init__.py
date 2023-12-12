3e55d6dfe73037f0e7ed2c809a298a64b859a568
darksim33 <57491748+darksim33@users.noreply.github.com>
2023-12-12 16:30:56 +0100

    Update __init__.py

--- src/ui/menubar/__init__.py
+++ src/ui/menubar/__init__.py
@@ -1,2 +1,4 @@
-from src.ui.menubar import filemenu
-from src.ui.menubar import editmenu
+from src.ui.menubar.filemenu import FileMenu
+from src.ui.menubar.editmenu import EditMenu
+from src.ui.menubar.fittingmenu import FittingMenu
+from src.ui.menubar.viewmenu import ViewMenu
