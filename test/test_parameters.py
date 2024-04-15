cdbd9e914adef7d88ccd9b7218b151e928477064
Thomas <57491748+darksim33@users.noreply.github.com>
2024-04-15 20:53:34 +0200

    Updated Parameters Test;

--- test/test_parameters.py
+++ test/test_parameters.py
@@ -1,20 +1,45 @@
 from pathlib import Path
-from pyneapple.fit.parameters import Parameters, NNLSParams, NNLSbaseParams
+from pyneapple.fit.parameters import Parameters, IVIMParams, NNLSParams, NNLSCVParams, IDEALParams
 
 
-def test_json_save(capsys):
-    params = Parameters(Path(r"resources/fitting/default_params_IVIM_tri.json"))
-    params.save_to_json(Path(r"test_params.json"))
-    params = NNLSbaseParams(Path(r"resources/fitting/default_params_NNLS.json"))
-    params.save_to_json(Path(r"test_params_nnls.json"))
-    params = NNLSParams(Path(r"resources/fitting/default_params_NNLS.json"))
-    params.save_to_json(Path(r"test_params_nnls_reg.json"))
-    capsys.readouterr()
+def test_init_parameters():
+    Parameters()
+    assert True
+
+
+def test_init_nnls_parameters():
+    NNLSParams()
+    assert True
+
+
+def test_init_nnls_cv_parameters():
+    NNLSCVParams()
+    assert True
+
+
+def test_init_ivim_parameters():
+    IVIMParams()
+    assert True
+
+
+def test_init_ideal_parameters():
+    IDEALParams()
     assert True
 
 
 def test_json_load():
-    Parameters().load_from_json(Path(r"resources/fitting/default_params_IVIM_tri.json"))
-    Parameters().load_from_json(Path(r"resources/fitting/default_params_NNLS.json"))
-    Parameters().load_from_json(Path(r"resources/fitting/default_params_NNLS.json"))
+    IVIMParams().load_from_json(Path(r"../src/pyneapple/resources/fitting/default_params_IVIM_tri.json"))
+    NNLSParams().load_from_json(Path(r"../src/pyneapple/resources/fitting/default_params_NNLS.json"))
+    NNLSCVParams().load_from_json(Path(r"../src/pyneapple/resources/fitting/default_params_NNLSCV.json"))
+    assert True
+
+
+def test_json_save(capsys):
+    params = IVIMParams(Path(r"../src/pyneapple/resources/fitting/default_params_IVIM_tri.json"))
+    params.save_to_json(Path(r"test_params.json"))
+    params = NNLSParams(Path(r"../src/pyneapple/resources/fitting/default_params_NNLS.json"))
+    params.save_to_json(Path(r"test_params_nnls.json"))
+    params = NNLSCVParams(Path(r"../src/pyneapple/resources/fitting/default_params_NNLSCV.json"))
+    params.save_to_json(Path(r"test_params_nnls_reg.json"))
+    capsys.readouterr()
     assert True
