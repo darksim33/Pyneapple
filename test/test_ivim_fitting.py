c4d3c6274e5cb976def757d1b7632cfadc0e37b8
thiel <57491748+darksim33@users.noreply.github.com>
2024-04-24 15:10:42 +0200

    Test Rework;

--- test/test_ivim_fitting.py
+++ test/test_ivim_fitting.py
@@ -2,6 +2,7 @@ import pytest
 from multiprocessing import freeze_support
 from functools import wraps
 from pathlib import Path
+import numpy as np
 
 from pyneapple.fit import FitData, parameters
 
@@ -17,7 +18,7 @@ def freeze_me(func):
 
 
 # Tests
-def test_tri_exp_segmented(ivim_tri_fit_data: FitData, capsys):
+def test_ivim_tri_segmented(ivim_tri_fit_data: FitData, capsys):
     ivim_tri_fit_data.fit_segmentation_wise()
     capsys.readouterr()
     assert True
@@ -43,5 +44,47 @@ def test_ivim_bi_pixel_multithreading(ivim_bi_fit_data: FitData, capsys):
 def test_ivim_tri_pixel_multithreading(ivim_tri_fit_data: FitData, capsys):
     ivim_tri_fit_data.fit_params.n_pools = 4
     ivim_tri_fit_data.fit_pixel_wise(multi_threading=True)
+    capsys.readouterr()
+    assert True
+
+
+def test_ivim_mono_pixel_sequential(ivim_mono_fit_data: FitData, capsys):
+    ivim_mono_fit_data.fit_pixel_wise(multi_threading=False)
+    capsys.readouterr()
+    assert True
+
+
+def test_ivim_bi_pixel_sequential(ivim_bi_fit_data: FitData, capsys):
+    ivim_bi_fit_data.fit_pixel_wise(multi_threading=False)
+    capsys.readouterr()
+    assert True
+
+
+def test_ivim_tri_pixel_sequential(ivim_tri_fit_data: FitData, capsys):
+    ivim_tri_fit_data.fit_pixel_wise(multi_threading=False)
+    capsys.readouterr()
+    assert True
+    return ivim_tri_fit_data
+
+
+def test_ivim_mono_result_to_fit_curve(ivim_mono_fit_data: FitData, capsys):
+    ivim_mono_fit_data.fit_results.raw[0, 0, 0] = np.array([0.15, 150])
+    ivim_mono_fit_data.fit_params.fit_model(
+        ivim_mono_fit_data.fit_params.b_values,
+        *ivim_mono_fit_data.fit_results.raw[0, 0, 0].tolist()
+    )
+    capsys.readouterr()
+    assert True
+
+
+def test_ivim_tri_result_to_nii(ivim_tri_fit_data: FitData, out_nii, capsys):
+    if not ivim_tri_fit_data.fit_results.d:
+        ivim_tri_fit_data = test_ivim_tri_pixel_sequential(ivim_tri_fit_data, capsys)
+
+    ivim_tri_fit_data.fit_results.save_fitted_parameters_to_nii(
+        out_nii,
+        ivim_tri_fit_data.img.array.shape,
+        parameter_names=ivim_tri_fit_data.fit_params.boundaries.parameter_names,
+    )
     capsys.readouterr()
     assert True
--- test/test_nnls_fitting.py
+++ test/test_nnls_fitting.py
@@ -1,10 +1,23 @@
 import pytest
 from pathlib import Path
+from functools import wraps
+from multiprocessing import freeze_support
 
 from pyneapple.utils.nifti import Nii, NiiSeg
 from pyneapple.fit import FitData
 
 
+# Decorators
+def freeze_me(func):
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        return func(*args, **kwargs)
+
+    freeze_support()
+    return wrapper
+
+
+# Segmented sequential fitting
 def test_nnls_segmented_reg_0(nnls_fit_data: FitData, out_nii: Path, capsys):
     nnls_fit_data.fit_params.reg_order = 0
     nnls_fit_data.fit_segmentation_wise()
@@ -42,3 +55,37 @@ def test_nnls_segmented_reg_cv(nnlscv_fit_data: FitData, out_nii: Path, capsys):
     nii_dyn.save(out_nii)
     capsys.readouterr()
     assert True
+
+
+# Multithreading
+@freeze_me
+def test_nnls_pixel_multi_reg_0(nnls_fit_data: FitData, capsys):
+    nnls_fit_data.fit_params.reg_order = 0
+    nnls_fit_data.fit_pixel_wise(multi_threading=True)
+    capsys.readouterr()
+    assert True
+
+
+@freeze_me
+def test_nnls_pixel_multi_reg_2(nnls_fit_data: FitData, capsys):
+    nnls_fit_data.fit_params.reg_order = 2
+    nnls_fit_data.fit_params.n_pools = 2
+    nnls_fit_data.fit_pixel_wise(multi_threading=True)
+    capsys.readouterr()
+    assert True
+
+
+@freeze_me
+def test_nnls_pixel_multi_reg_3(nnls_fit_data: FitData, capsys):
+    nnls_fit_data.fit_params.reg_order = 3
+    nnls_fit_data.fit_params.n_pools = 2
+    nnls_fit_data.fit_pixel_wise(multi_threading=True)
+    capsys.readouterr()
+    assert True
+
+
+# @freeze_me
+# def test_nnls_pixel_multi_reg_cv(nnlscv_fit_data: FitData, capsys):
+#     nnlscv_fit_data.fit_pixel_wise(multi_threading=True)
+#     capsys.readouterr()
+#     assert True
