a4cd545d4ad91bf779e54586d236aa73f07402fb
thiel <57491748+darksim33@users.noreply.github.com>
2024-04-29 16:55:34 +0200

    Changed some files and added a ui marker;

--- test/test_ui.py
+++ test/test_ui.py
@@ -1,9 +1,11 @@
 import sys
 from PyQt6 import QtWidgets
+import pytest
 
 from pyneapple.ui.dlg_prompts import ReshapeSegMessageBox
 
 
+@pytest.mark.ui
 def test_reshape_seg_dlg(img, seg):
     app = QtWidgets.QApplication(sys.argv)
     dialog = ReshapeSegMessageBox(debug=True)
--- test/test_utils.py
+++ test/test_utils.py
@@ -4,25 +4,9 @@ from pathlib import Path
 from pyneapple.utils.nifti import Nii, NiiSeg
 
 
-def test_nii():
-    img = Nii(Path(r"../data/test_img.nii"))
+def test_nii_zero_padding(img):
+    img = Nii(Path(r".data/test_img_176x128.nii"), do_zero_padding=True)
     if img.path:
-        assert True
-    else:
-        assert False
-
-
-def test_nii_zero_padding():
-    img = Nii(Path(r"../data/test_img_176_128.nii"), do_zero_padding=True)
-    if img.path:
-        assert True
-    else:
-        assert False
-
-
-def test_nii_seg():
-    seg = NiiSeg(Path(r"../data/test_mask_128.nii.gz"))
-    if seg.path:
         assert True
     else:
         assert False
