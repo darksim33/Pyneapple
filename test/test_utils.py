6896b59dae3404d817d6ee502270e2f23f2b5852
thiel <57491748+darksim33@users.noreply.github.com>
2024-05-06 14:43:50 +0200

    Added save mean signal to excel and corresponding test;

--- test/test_utils.py
+++ test/test_utils.py
@@ -1,7 +1,9 @@
 import pytest
+import pandas as pd
 
 from pathlib import Path
 from pyneapple.utils.nifti import Nii, NiiSeg
+from test_results import compare_lists
 
 
 def test_nii(img):
@@ -14,3 +16,16 @@ def test_nii_zero_padding(img):
         assert True
     else:
         assert False
+
+
+def test_save_mean_signal_to_excel(img, seg, nnls_params, out_excel):
+    if out_excel.is_file():
+        out_excel.unlink()
+
+    seg.save_mean_signals_to_excel(img, nnls_params.b_values, out_excel)
+    df = pd.read_excel(out_excel)
+    columns = df.columns.tolist()
+    if len(columns) > 16:
+        columns = columns[1:]
+    b_values = nnls_params.b_values.squeeze().tolist()
+    compare_lists(columns, b_values)
