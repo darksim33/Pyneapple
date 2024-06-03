6469c9eefb3bb3c5f5f4220f51a7738b50a6db07
thiel <57491748+darksim33@users.noreply.github.com>
2024-06-03 16:42:20 +0200

    chore: fixture rename append

--- test/test_nnls_parameters.py
+++ test/test_nnls_parameters.py
@@ -31,7 +31,7 @@ class TestNNLSParameters:
         assert args is not None
 
     def test_nnls_eval_fitting_results(
-        self, nnls_fit_results, nnls_params, nii_seg_reduced
+        self, nnls_fit_results, nnls_params, seg_reduced
     ):
         results = nnls_params.eval_fitting_results(nnls_fit_results[0])
         fit_results = Results()
@@ -45,7 +45,7 @@ class TestNNLSParameters:
     #     assert True
 
     @pytest.mark.order(after="test_nnls_eval_fitting_results")
-    def test_nnls_apply_auc(self, nnls_params, nnls_fit_results, nii_seg_reduced):
+    def test_nnls_apply_auc(self, nnls_params, nnls_fit_results, seg_reduced):
         results = nnls_params.eval_fitting_results(nnls_fit_results[0])
         fit_results = Results()
         fit_results.update_results(results)
