702eb2ff0e17e06b689556dc5a4cffe2c4700a29
darksim33 <57491748+darksim33@users.noreply.github.com>
2024-09-03 15:39:20 +0200

    fix: rework evaluation to better suite segmented fitting needs

--- src/pyneapple/fit/parameters.py
+++ src/pyneapple/fit/parameters.py
@@ -597,23 +597,15 @@ class IVIMParams(Parameters):
                 Each entry holds a dictionary containing the different results.
         """
         # prepare arrays
-        raw, S0, d, f, curve = dict(), dict(), dict(), dict(), dict()
-        if self.TM:
-            t_one = dict()
+        raw, S0, d, f, curve, t_one = dict(), dict(), dict(), dict(), dict(), dict()
 
         for element in results:
-            raw[element[0]] = element[1]
-            S0[element[0]] = element[1][-1]  # TODO: is this appropriate with S/S0?
-            d[element[0]] = element[1][0 : self.n_components]
-
+            S0[element[0]] = self.get_s_0_values_from_results(element[1])
             f[element[0]] = self.get_fractions_from_results(element[1])
-
-            # add curve fit
+            d[element[0]] = self.get_diffusion_values_from_results(element[1])
+            raw[element[0]] = self.get_raw_result(element[1])
             curve[element[0]] = self.fit_model(self.b_values, *element[1])
-
-            # add additional T1 results if necessary
-            if self.TM:
-                t_one[element[0]] = [element[1][1]]
+            t_one[element[0]] = self.get_t_one_from_results(element[1])
 
         spectrum = self.set_spectrum_from_variables(d, f)
 
@@ -643,8 +635,27 @@ class IVIMParams(Parameters):
             f_new[-1] = 1 - np.sum(f_new)
         return f_new
 
-    def get_diffusion_values_from_results(self, results: np.ndarray):
+    def get_diffusion_values_from_results(self, results: np.ndarray, **kwargs):
         return results[: self.n_components]
+
+    def get_s_0_values_from_results(self, results: np.ndarray):
+        if isinstance(self.scale_image, str) and self.scale_image == "S/S0":
+            return 1
+        else:
+            if self.TM:
+                return results[-2]
+            else:
+                return results[-1]
+
+    def get_t_one_from_results(self, results: np.ndarray):
+        if self.TM:
+            return results[-1]
+        else:
+            return None
+
+    @staticmethod
+    def get_raw_result(results: np.ndarray, **kwargs):
+        return results  # for the normal ivim model all variables are returned at once
 
     def set_spectrum_from_variables(
         self, d: dict, f: dict, number_points: int | None = None
@@ -980,8 +991,9 @@ class IVIMSegmentedParams(IVIMParams):
         ----------
             results
                 Pass the results of the fitting process to this function
-            fixed_component: list
-                Dictionary holding results from first fitting step
+            kwargs
+                fixed_component: list(dict, dict)
+                    Dictionary holding results from first fitting step
 
         Returns
         ----------
@@ -990,8 +1002,58 @@ class IVIMSegmentedParams(IVIMParams):
                 Each entry holds a dictionary containing the different results.
         """
 
+        fixed_component = kwargs.get("fixed_component", [[None]])
+
+        raw, S0, d, f, curve, t_one = dict(), dict(), dict(), dict(), dict(), dict()
+
+        for element in results:
+            S0[element[0]] = self.get_s_0_values_from_results(element[1])
+            f[element[0]] = self.get_fractions_from_results(element[1])
+            d[element[0]] = self.get_diffusion_values_from_results(
+                element[1], fixed_component=fixed_component[0][element[0]]
+            )
+            raw[element[0]] = self.get_raw_result(
+                element[1], fixed_component=fixed_component[0][element[0]]
+            )
+            curve[element[0]] = self.fit_model(self.b_values, *element[1])
+            t_one[element[0]] = [element[1][1]]
+
+        spectrum = self.set_spectrum_from_variables(d, f)
+
+        fit_results = {
+            "raw": raw,
+            "S0": S0,
+            "d": d,
+            "f": f,
+            "curve": curve,
+            "spectrum": spectrum,
+        }
+        if self.TM:
+            fit_results.update({"T1": t_one})
+
+        return fit_results
+
+    def get_diffusion_values_from_results(self, results: np.ndarray, **kwargs):
+        """
+        Returns the diffusion values from the results and adds the fixed component to the results.
+
+        Args:
+            results: np.ndarray containing the fitting results
+        kwargs:
+            fixed_component: list | np.ndarray containing the fixed component results
+
+        Returns:
+            d_new: np.ndarray containing the diffusion values
+        """
         fixed_component = kwargs.get("fixed_component", None)
-        # TODO: rework basic function in a way that altering sub functions can be utilized
+
+        d_new = np.zeros(self.n_components)
+        # add fixed component
+        d_new[0] = fixed_component
+        # since D_slow aka ADC is the default fitting parameter it is always at 0
+        # this will cause issues if the fixed component is not the first component
+        d_new[1:] = results[: self.n_components]
+        return d_new
 
 
 class IDEALParams(IVIMParams):
