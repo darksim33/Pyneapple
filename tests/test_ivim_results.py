ddf3ffbdf70734c98f2e536f8ed1d91d4ea96db9
Thomas <57491748+darksim33@users.noreply.github.com>
2024-11-08 13:17:07 +0100

    feat: add tests excel, nii, heatmap

--- tests/test_ivim_results.py
+++ tests/test_ivim_results.py
@@ -1,7 +1,6 @@
-import pandas as pd
-import numpy as np
-
 from pyneapple import IVIMResults
+
+from .test_results import save_spectrum_to_excel, save_curve_to_excel
 
 
 def test_eval_results(ivim_bi_params, results_bi_exp):
@@ -21,9 +20,50 @@ def test_get_spectrum():
     pass  # TODO: Implement test
 
 
+def test_save_spectrum_to_excel(ivim_bi_params, array_result, out_excel):
+    result = IVIMResults(ivim_bi_params)
+    save_spectrum_to_excel(array_result, out_excel, result)
+
+
+def test_save_curve_to_excel(ivim_bi_params, array_result, out_excel):
+    result = IVIMResults(ivim_bi_params)
+    save_curve_to_excel(array_result, out_excel, result)
+
+
 def test_save_to_nii(root, ivim_bi_params, results_bi_exp, img):
     file_path = root / "tests" / ".out" / "test"
     results = IVIMResults(ivim_bi_params)
     results.eval_results(results_bi_exp)
     results.save_to_nii(file_path, img)
+
     assert (file_path.parent / (file_path.stem + "_d.nii.gz")).is_file()
+    assert (file_path.parent / (file_path.stem + "_f.nii.gz")).is_file()
+    assert (file_path.parent / (file_path.stem + "_s0.nii.gz")).is_file()
+    assert (file_path.parent / (file_path.stem + "_t1.nii.gz")).is_file()
+
+    # TODO: Check if the files are correct
+
+    results.save_to_nii(file_path, img, separate_files=True)
+    for idx in range(2):
+        assert (file_path.parent / (file_path.stem + f"_d_{idx}.nii.gz")).is_file()
+        assert (file_path.parent / (file_path.stem + f"_f_{idx}.nii.gz")).is_file()
+    assert (file_path.parent / (file_path.stem + f"_s0.nii.gz")).is_file()
+
+    for file in file_path.parent.glob("*.nii.gz"):
+        file.unlink()
+
+
+def test_save_to_heatmap(root, ivim_bi_params, results_bi_exp, img):
+    file_path = root / "tests" / ".out" / "test"
+    results = IVIMResults(ivim_bi_params)
+    results.eval_results(results_bi_exp)
+    n_slice = 0
+    results.save_heatmap(file_path, img, n_slice)
+
+    for idx in range(2):
+        assert (file_path.parent / (file_path.stem + f"_{n_slice}_d_{idx}.png")).is_file()
+        assert (file_path.parent / (file_path.stem + f"_{n_slice}_f_{idx}.png")).is_file()
+    assert (file_path.parent / (file_path.stem + f"_{n_slice}_s_0.png")).is_file()
+
+    for file in file_path.parent.glob("*.png"):
+        file.unlink()
