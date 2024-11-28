28a818d12b2fe011dfc369df6d1de70ebf213ca7
darksim33 <57491748+darksim33@users.noreply.github.com>
2024-11-28 18:59:36 +0100

    feat: init gpu tests

--- tests/test_pygpufit.py
+++ tests/test_pygpufit.py
@@ -1,11 +1,83 @@
+import pytest
+
+import numpy as np
+from pyneapple.fitting.gpubridge import gpu_fitter
+
+
+@pytest.mark.gpu
 def test_import_gpufit():
     import pygpufit
 
     assert pygpufit is not None
 
 
-def test_cuda_check():
+@pytest.fixture
+def gpufit():
     import pygpufit.gpufit as gpufit
+
+    return gpufit
+
+
+@pytest.mark.gpu
+def test_cuda_check(gpufit):
 
     gpufit.cuda_available()
     assert True
+
+
+@pytest.mark.gpu
+def test_models_available(gpufit):
+    models = [
+        "MONO_EXP",
+        "MONO_EXP_RED",
+        "BI_EXP",
+        "BI_EXP_RED",
+        "TRI_EXP",
+        "TRI_EXP_RED",
+    ]
+    for model in models:
+        assert getattr(gpufit.ModelID, model, None) is not None
+
+
+@pytest.mark.gpu
+def test_gpu_fit_mono(gpufit, ivim_mono_params):
+    n_fits = 100
+    ivim_mono_params.scale_image = "S/S0"
+    fit_args = np.zeros((n_fits, ivim_mono_params.b_values.shape[0]), np.float32)
+    # weights = np.ones((n_fits, ivim_mono_params.b_values.shape[0]), np.float32)
+    d_values = np.random.uniform(0.0007, 0.003, (n_fits, 1))
+    start_values = np.full(
+        (n_fits, 1), ivim_mono_params.boundaries.start_values, dtype=np.float32
+    )
+    constraints = np.tile(
+        np.concatenate(
+            (
+                ivim_mono_params.boundaries.lower_stop_values,
+                ivim_mono_params.boundaries.upper_stop_values,
+            ),
+            dtype=np.float32,
+        ),
+        (n_fits, 1),
+    )
+    constraint_types = np.tile(np.int32(gpufit.ConstraintType.LOWER_UPPER), (1, 1))
+    b_values = np.tile(ivim_mono_params.b_values.T, (n_fits, 1))
+
+    for n_fit in range(n_fits):
+        fit_args[n_fit, :] = np.squeeze(
+            np.exp(-ivim_mono_params.b_values * d_values[n_fit])
+        )
+
+    result = gpufit.fit_constrained(
+        fit_args,
+        None,
+        gpufit.ModelID.MONO_EXP_RED,
+        initial_parameters=start_values,
+        constraints=constraints,
+        constraint_types=constraint_types,
+        tolerance=1e-3,
+        max_number_iterations=250,
+        parameters_to_fit=np.array([1], np.int32),
+        estimator_id=gpufit.EstimatorID.LSE,
+        user_info=b_values,
+    )
+    assert result is not None
