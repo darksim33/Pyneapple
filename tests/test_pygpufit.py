26b3143b205fe4eea9bfd4d9e8ccc27181aaf65c
darksim33 <57491748+darksim33@users.noreply.github.com>
2024-12-02 21:13:54 +0100

    feat: add tri exp test for gpu fit

--- tests/test_pygpufit.py
+++ tests/test_pygpufit.py
@@ -81,3 +81,37 @@ def test_gpu_fit_mono(gpufit, ivim_mono_params):
         user_info=b_values,
     )
     assert result is not None
+
+
+@pytest.mark.gpu
+def test_gpu_fit_tri(gpufit, decay_tri, ivim_tri_params):
+    fit_data = decay_tri["fit_array"]
+    n_fits = fit_data.shape[0]
+
+    starts = [210, 0.001, 210, 0.02, 210, 0.01]
+    lower = [10, 0.0007, 10, 0.003, 10, 0.003]
+    upper = [2500, 0.05, 2500, 0.05, 2500, 0.3]
+    start_values = np.tile(np.float32(starts), (n_fits, 1))
+    constraints = np.tile(
+        np.float32(list(zip(lower, upper))).flatten(),
+        (n_fits, 1),
+    )
+    b_values = np.tile(np.char.array(ivim_tri_params.b_values.T), (n_fits, 1))
+    constraint_types = np.squeeze(
+        np.tile(np.int32(gpufit.ConstraintType.LOWER_UPPER), (6, 1))
+    )
+
+    result = gpufit.fit_constrained(
+        fit_data,
+        None,
+        13,
+        initial_parameters=start_values,
+        constraints=constraints,
+        constraint_types=constraint_types,
+        tolerance=0.000001,
+        max_number_iterations=250,
+        parameters_to_fit=None,
+        estimator_id=gpufit.EstimatorID.LSE,
+        user_info=b_values,
+    )
+    assert (result[0][0, :] != np.array(starts)).all()
