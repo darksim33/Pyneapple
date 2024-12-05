a1b588d38d7d3b81d04cb92bece16b396fb11a8d
darksim33 <57491748+darksim33@users.noreply.github.com>
2024-12-05 16:07:07 +0100

    feat: enable gpu fitting. fix parameter input types

--- src/pyneapple/fitting/gpubridge.py
+++ src/pyneapple/fitting/gpubridge.py
@@ -2,16 +2,34 @@ from __future__ import annotations
 
 import numpy as np
 
-from pygpufit import gpufit as gf
+from pygpufit import gpufit as gpufit
 from .. import IVIMParams, IVIMSegmentedParams
 
 
-def gpu_fitter(data: zip, params: IVIMParams | IVIMSegmentedParams):
+def reorder_array(array: np.ndarray) -> np.ndarray:
+    """Adjust Oder to fit GPU fitting Models.
+    From D1,D2,...F1,F2,... to F1,D1,F2,D2,... for GPU fitting.
+    """
+    n = len(array)
+    if n % 2 != 0:
+        raise ValueError("Array length must be even.")
+    reordered = []
+    half = n // 2
+    for i in range(half):
+        reordered.append(array[half + i])
+        reordered.append(array[i])
+    return np.array(reordered)
+
+
+def gpu_fitter(data: zip, params: IVIMParams | IVIMSegmentedParams, **kwargs):
 
     if isinstance(data, zip):
-        data_list = list()
-        for data_tuple in data:
-            data_list.append(data_tuple[1])
+        pixel_indices, data_list = [], []
+        for element in data:
+            pixel_indices.append(element[0])
+            data_list.append(element[1])
+        # pixel_indices = [element[0] for element in data]
+        # data_list = [element[1] for element in data]
         fit_data = np.array(data_list)
     else:
         raise ValueError("Data for GPU fitting must be zipped.")
@@ -25,24 +43,40 @@ def gpu_fitter(data: zip, params: IVIMParams | IVIMSegmentedParams):
         fit_model = "TRI_EXP"
     else:
         raise ValueError("Invalid number of components for GPU fitting.")
-    if params.scale_images == "S/S0":
+    if params.scale_image == "S/S0":
         fit_model += "_RED"
         n_parameters -= 1
 
-    start_values = np.zeros((fit_data.shape[0], params.n_components))
-    constraints = np.zeros((fit_data.shape[0], params.n_components * 2))
-    constraint_types = np.zeros((n_parameters, 1))
-    b_values = np.zeros((fit_data.shape[0], params.b_values.shape[0]))
-    for n_fit in range(fit_data.shape[0]):
-        start_values[n_fit, :] = params.boundaries.start_values
-        constraints[n_fit, 0::2] = params.boundaries.lower_bounds
-        constraints[n_fit, 1::2] = params.boundaries.upper_bounds
-        constraint_types[n_fit] = gf.ConstraintType.LOWER_UPPER
-        b_values[n_fit, :] = params.b_values
+    fit_model = getattr(gpufit.ModelID, fit_model, None)
+    if fit_model is None:
+        raise ValueError("Invalid model for GPU fitting.")
 
-    tolerance = 1e-6  # TODO: Add parameter for tolerance
+    start_values = np.tile(
+        reorder_array(params.boundaries.start_values.astype(np.float32)),
+        (fit_data.shape[0], 1),
+    )
 
-    result = gf.fit_constrained(
+    constraints = np.tile(
+        np.float32(
+            list(
+                zip(
+                    reorder_array(params.boundaries.lower_stop_values),
+                    reorder_array(params.boundaries.upper_stop_values),
+                )
+            )
+        ).flatten(),
+        (fit_data.shape[0], 1),
+    )
+    constraint_types = np.squeeze(
+        np.tile(np.int32(gpufit.ConstraintType.LOWER_UPPER), (n_parameters, 1))
+    )
+    b_values = np.squeeze(params.b_values).astype(np.float32)
+
+    tolerance = getattr(kwargs, "tolerance", 1e-6)  # TODO: Add parameter for tolerance
+    parameters_to_fit = getattr(kwargs, "parameters_to_fit", None)
+    estimator = getattr(kwargs, "estimator", gpufit.EstimatorID.LSE)
+
+    result = gpufit.fit_constrained(
         fit_data,
         None,
         fit_model,
@@ -51,7 +85,12 @@ def gpu_fitter(data: zip, params: IVIMParams | IVIMSegmentedParams):
         constraint_types=constraint_types,
         tolerance=tolerance,
         max_number_iterations=params.max_iter,
-        parameters_to_fit=np.int32(n_parameters),
-        estimator_id=gf.EstimatorID.LSE,
+        parameters_to_fit=parameters_to_fit,  # NOTE: What happens if the number of parameters is reduced?
+        estimator_id=estimator,
         user_info=b_values,
     )
+    fit_results = [
+        (pixel_indices[i], result[0][i, :]) for i in range(len(pixel_indices))
+    ]
+
+    return fit_results  # NOTE: export fit quality parameters
