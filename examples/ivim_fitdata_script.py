bf437d88fe37fd1b482d3036d62d3b8ff5738f6c
darksim33 <thomas.thiel92@gmail.com>
2025-10-16 16:12:08 +0200

    feat: update example scripts and documentation links

--- examples/ivim_fitdata_script.py
+++ examples/ivim_fitdata_script.py
@@ -13,11 +13,11 @@ def main():
     #  Set Working Directory for file processing
     working_dir = Path(__file__).parent
     # Load the DWI image and segmentation data
-    img = RadImgArray(working_dir / "tests" / ".data" / "ivim_data.nii.gz")
-    seg = SegImgArray(working_dir / "tests" / ".data" / "ivim_segmentation.nii.gz")
+    img = RadImgArray(working_dir / "examples" / "images" / "test_img.nii.gz")
+    seg = SegImgArray(working_dir / "examples" / "images" / "test_seg.nii.gz")
 
     # Load the IVIM fitting parameters from a JSON or TOML file
-    json = working_dir / "examples" / "fitting" / "ivim_biexp_params.json"
+    json = working_dir / "examples" / "parameters" / "ivim_biexp_params.json"
 
     # Initiate data object with the image, segmentation and parameters
     data = pyneapple.FitData(
@@ -31,3 +31,7 @@ def main():
     data.results.save_to_excel(working_dir / "ivim_results.xlsx")
     # to recover original image space information use img Array
     data.results.save_to_nii(working_dir / "ivim_results", img)
+
+
+if __name__ == "__main__":
+    main()
