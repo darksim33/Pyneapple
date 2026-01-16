53c909a983f916e6286e785a9781e5baf92f9680
darksim33 <57491748+darksim33@users.noreply.github.com>
2026-01-16 12:36:29 +0100

    docs: fix docstring formats

--- src/pyneapple/io/nifti.py
+++ src/pyneapple/io/nifti.py
@@ -8,20 +8,11 @@ from ..utils.logger import logger
 def save_to_nifti(img: RadImgArray, filename: Path, **kwargs):
     """Save a RadImgArray to a NIfTI file.
 
-    Parameters
-    ----------
-    img : RadImgArray
-        The image to save.
-    filename : str
-        The filename to save the image to.
-    **kwargs
-        Additional keyword arguments to pass to the save method.
-        dtype : object
-            The data type to save the image as.
-
-    Returns
-    -------
-    None
+    Args:
+        img (RadImgArray): The image to save.
+        filename (str): The filename to save the image to.
+        **kwargs
+            dtype (object): The data type to save the image as.
     """
     img.save(filename, save_as="nii", **kwargs)
     logger.info(f"Saved {filename}")
@@ -30,20 +21,12 @@ def save_to_nifti(img: RadImgArray, filename: Path, **kwargs):
 def arrays_to_nifti(imgs: list[RadImgArray], filenames: list[Path], **kwargs):
     """Save a list of RadImgArray to a NIfTI file.
 
-    Parameters
-    ----------
-    imgs : list[RadImgArray]
-        The images to save.
-    filename : str
-        The filename to save the images to.
-    **kwargs
-        Additional keyword arguments to pass to the save method.
-        dtype : object
-            The data type to save the images as.
+    Args:
+        imgs (list[RadImgArray]): The images to save.
+        filename (str): The filename to save the images to.
+        **kwargs
+            dtype (object): The data type to save the images as.
 
-    Returns
-    -------
-    None
     """
     for img, filename in zip(imgs, filenames):
         save_to_nifti(img, filename, **kwargs)
