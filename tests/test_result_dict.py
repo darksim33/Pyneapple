08560b1bc48e9ae848825cc52c144a7c740e751e
darksim33 <57491748+darksim33@users.noreply.github.com>
2026-01-20 13:32:31 +0100

    test:refactor: Improve test structure and add detailed docstrings for ResultDict tests

--- tests/test_result_dict.py
+++ tests/test_result_dict.py
@@ -17,79 +17,83 @@ from pyneapple.results.result_dict import ResultDict
 from radimgarray import RadImgArray
 
 
-def test_get():
-    result_dict = ResultDict()
-    result_dict[(1, 1, 1)] = 1.1
+class TestResultDict:
+    """Test suite for ResultDict basic operations and conversions."""
 
-    assert result_dict[(1, 1, 1)] == 1.1
-    assert result_dict.get((1, 1, 1), 0) == 1.1
+    def test_get(self):
+        """Test that dictionary get operations work correctly with coordinate tuple keys."""
+        result_dict = ResultDict()
+        result_dict[(1, 1, 1)] = 1.1
 
+        assert result_dict[(1, 1, 1)] == 1.1
+        assert result_dict.get((1, 1, 1), 0) == 1.1
 
-def test_get_seg():
-    result_dict = ResultDict()
-    result_dict[(1, 1, 1)] = 1.1
-    result_dict.set_segmentation_wise({(1, 1, 1): 1})
-    result_dict[1] = 1.1
+    def test_get_seg(self):
+        """Test that segmentation-wise storage and retrieval works with both coordinate and segment keys."""
+        result_dict = ResultDict()
+        result_dict[(1, 1, 1)] = 1.1
+        result_dict.set_segmentation_wise({(1, 1, 1): 1})
+        result_dict[1] = 1.1
 
-    assert result_dict[(1, 1, 1)] == 1.1
-    assert result_dict.get((1, 1, 1), 0) == 1.1
+        assert result_dict[(1, 1, 1)] == 1.1
+        assert result_dict.get((1, 1, 1), 0) == 1.1
 
+    def test_validate_key(self):
+        """Test that keys are properly validated and converted to correct types."""
+        result_dict = ResultDict()
+        # result_dict[(1, 1, 1)] = 1.1
+        # result_dict.set_segmentation_wise({(1, 1, 1): 1})
+        result_dict[np.int32(1)] = 1.1
+        for key in result_dict:
+            assert isinstance(key, int)
 
-def test_validate_key():
-    result_dict = ResultDict()
-    # result_dict[(1, 1, 1)] = 1.1
-    # result_dict.set_segmentation_wise({(1, 1, 1): 1})
-    result_dict[np.int32(1)] = 1.1
-    for key in result_dict:
-        assert isinstance(key, int)
+    def test_update(self):
+        """Test that update method correctly merges multiple dictionaries with coordinate keys."""
+        values_1 = {(1, 1, 1): 1.5}
+        values_2 = {(0, 0, 0): 1.7}
+        result_dict = ResultDict()
+        result_dict.update(values_1)
+        assert result_dict[(1, 1, 1)] == 1.5
+        result_dict.update(values_2)
+        assert result_dict[(0, 0, 0)] == 1.7
 
+    def test_update_with_seg(self):
+        """Test that update method works correctly with segment number keys."""
+        value_1 = {1: 1.5}
+        value_2 = {2: 1.7}
+        result_dict = ResultDict()
+        result_dict.update(value_1)
+        assert result_dict[1] == 1.5
+        result_dict.update(value_2)
+        assert result_dict[2] == 1.7
 
-def test_update():
-    values_1 = {(1, 1, 1): 1.5}
-    values_2 = {(0, 0, 0): 1.7}
-    result_dict = ResultDict()
-    result_dict.update(values_1)
-    assert result_dict[(1, 1, 1)] == 1.5
-    result_dict.update(values_2)
-    assert result_dict[(0, 0, 0)] == 1.7
+    def test_as_array(self):
+        """Test that as_array converts ResultDict to numpy array with correct shape and values."""
+        result_dict = ResultDict()
+        test_array = np.array([[1.3, 2.2], [3.1, 4.4]])
+        result_dict.update(
+            {
+                (0, 0): test_array[0, 0],
+                (0, 1): test_array[0, 1],
+                (1, 0): test_array[1, 0],
+                (1, 1): test_array[1, 1],
+            }
+        )
+        array = result_dict.as_array((2, 2, 1, 1))
+        assert np.allclose(array, test_array[..., np.newaxis, np.newaxis])
 
-
-def test_update_with_seg():
-    value_1 = {1: 1.5}
-    value_2 = {2: 1.7}
-    result_dict = ResultDict()
-    result_dict.update(value_1)
-    assert result_dict[1] == 1.5
-    result_dict.update(value_2)
-    assert result_dict[2] == 1.7
-
-
-def test_as_array():
-    result_dict = ResultDict()
-    test_array = np.array([[1.3, 2.2], [3.1, 4.4]])
-    result_dict.update(
-        {
-            (0, 0): test_array[0, 0],
-            (0, 1): test_array[0, 1],
-            (1, 0): test_array[1, 0],
-            (1, 1): test_array[1, 1],
-        }
-    )
-    array = result_dict.as_array((2, 2, 1, 1))
-    assert np.allclose(array, test_array[..., np.newaxis, np.newaxis])
-
-
-def test_as_rad_img_array():
-    result_dict = ResultDict()
-    test_array = RadImgArray(np.array([[1.3, 2.2], [3.1, 4.4]]))
-    result_dict.update(
-        {
-            (0, 0, 0, 0): test_array[0, 0],
-            (0, 1, 0, 0): test_array[0, 1],
-            (1, 0, 0, 0): test_array[1, 0],
-            (1, 1, 0, 0): test_array[1, 1],
-        }
-    )
-    test_array = test_array[:, :, np.newaxis, np.newaxis]
-    array = result_dict.as_RadImgArray(test_array)
-    assert np.allclose(array, test_array)
+    def test_as_rad_img_array(self):
+        """Test that as_RadImgArray converts ResultDict to RadImgArray with correct shape and values."""
+        result_dict = ResultDict()
+        test_array = RadImgArray(np.array([[1.3, 2.2], [3.1, 4.4]]))
+        result_dict.update(
+            {
+                (0, 0, 0, 0): test_array[0, 0],
+                (0, 1, 0, 0): test_array[0, 1],
+                (1, 0, 0, 0): test_array[1, 0],
+                (1, 1, 0, 0): test_array[1, 1],
+            }
+        )
+        test_array = test_array[:, :, np.newaxis, np.newaxis]
+        array = result_dict.as_RadImgArray(test_array)
+        assert np.allclose(array, test_array)
